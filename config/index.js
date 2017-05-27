a51c7e6b94f6cad5ae14a82f524edacc18beb208
[yangyi] <[639293382@qq.com]>
2017-05-27 11:42:13 +0800

    第一版本完成

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../todo/index.html'),
     assetsRoot: path.resolve(__dirname, '../todo'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
