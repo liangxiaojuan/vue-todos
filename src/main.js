48826f6c987a28f40fc316f6964deba8e405e807
[yangyi] <[639293382@qq.com]>
2017-06-05 15:20:39 +0800

    new

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+//  The Vue build version to load with the `import` command
+//  (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'; // 引入vue模块
+import App from './App'; // 引入vue组件
+import router from './router'; //  引入路由配置文件
 import './common/style/icon.less';
 import Mock from './mock';
 import store from './vuex/store';
 Mock.bootstrap();
-Vue.config.productionTip = false;
+Vue.config.productionTip = false;  //  关闭生产模式下给出的提示
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
+new Vue({ //  创建一个 Vue 的根实例
+  el: '#app', // 绑定id,这个实例下所有的内容都会在index.html 一个id为app的div下显示
+  router, //  注入路由配置
   store,
-  template: '<App/>',
+  template: '<App/>',  // 配置根模板 即打开页面显示那个组件
   components: {
-    App
+    App  //  注入组件
   }
 });
